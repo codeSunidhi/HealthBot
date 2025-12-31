bb090f192aeeef994108265ead05df2a1abb869e
thecode_sunidhi <sunidhiraturi1@gmail.com>
2025-12-31 17:25:11 +0530

    Cleanup before upload

--- CodeCrew_HealthSaathi/app/health-ai-backend/tunnel.py
+++
@@ -1,7 +0,0 @@
-from pyngrok import ngrok
-
-# Expose port 8000 to the internet
-public_url = ngrok.connect(8000)
-print("🌍 Public URL:", public_url)
-print("Your FastAPI app is live! Press CTRL+C to stop.")
-input("Press Enter to keep it running...")
